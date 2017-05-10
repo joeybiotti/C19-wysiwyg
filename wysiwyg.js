ffa4c3696a95a1407e12520221a93f4ffd90441b
Joey <joeybb@MacBook-Pro.local>
2017-05-09 22:42:32 -0500

    updated functions

--- wysiwyg.js
+++ wysiwyg.js
@@ -3,17 +3,16 @@ console.log("famous people", famousPeople);
 
 var famousPeople = [
 
-{
-  title: "Samurai",
-  name: "Tomoe Gozen",
-  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
-  lifespan: {
-    birth: 1747,
-    death: 1797
-  }
-},
     {
+        title: "Samurai",
+        name: "Tomoe Gozen",
+        bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+        lifespan: {
+            birth: 1747,
+            death: 1797
+        }
+    }, {
         title: "Baseball Player",
         name: "Ted Williams",
         bio: "Teddy Ballgame. Greatest hitter of all time.",
@@ -50,29 +49,53 @@ var famousPeople = [
         name: "Jack Kerouac",
         bio: "Author of On the Road. Member of the Beat Generation",
         image: "img/kerouac.png",
-        lifespan: { birth: 1922,
-                    death: 1969
+        lifespan: {
+            birth: 1922,
+            death: 1969
         }
     }
 ];
 
-console.log("famousPeople", famousPeople) //return the famous people.
+console.log("famousPeople", famousPeople);
 
 
 var person = " ";
 var people = document.getElementById("people");
-for(prop in famousPeople){
-    var person = `<article>
-                    <h1 class="title">${famousPeople[prop].title}</h1>
-                    <h3 class="name">${famousPeople[prop].name}<h3>
-                    <img class="image" src="${famousPeople[prop].image}" height="125" width="125">
-                    <section class="bio">${famousPeople[prop].bio}</section>
-                    <footer class="footer">${famousPeople[prop].lifespan.birth} to ${famousPeople[prop].lifespan.death}</footer>
+for (x in famousPeople) {
+    var person = `<article class="people">
+                    <h1 class="title">${famousPeople[x].title}</h1>
+                    <h3 class="name">${famousPeople[x].name}<h3>
+                    <img class="image" src="${famousPeople[x].image}" height="125" width="125">
+                    <section class="bio">${famousPeople[x].bio}</section>
+                    <footer class="footer">${famousPeople[x].lifespan.birth} to ${famousPeople[x].lifespan.death}</footer>
                 </article>`
 
     people.innerHTML += person;
 
 
-
 };
+
+
+// var input = document.getElementById("text-area");
+// var thisPerson = " ";
+
+// function focusText(){
+//     input.focus();
+// }
+
+// function clearInput(){
+//     input.value =" ";
+// }
+
+// input.addEventListener("keyup", function(event) {
+//     //key event
+//     thisPerson.innerHTML = event.target.value;
+//     if(key === 13){
+//         clearInput();
+//     }
+// });
+
+
+
+
 
